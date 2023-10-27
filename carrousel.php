94ef5d94d763f98b3ec15d6f295d5863db318899
Stephanie Guerrier <e1574836@cmaisonneuve.qc.ca>
2023-10-26 23:00:09 -0400

    Affichage thumbnails des profs galerie

--- carrousel.php
+++ carrousel.php
@@ -2,9 +2,9 @@
 
 /**
  * Plugin name: Carrousel
- * Author: Taïsha Dorval-Mbele
- * Author URI: https://github.com/xshujo
- * Description: Cette extension carrousel permettra d'afficher dans une boîte modale animée les images d'une galerie
+ * Author: DiversiTIM
+ * Author URI: https://cidweb38.sg-host.com/
+ * Description: Cette extension carrousel permettra d'afficher une galerie d'image qu'on peut faire défiler horizontalement
  * Version: 1.0.0
  */
 
@@ -32,15 +32,47 @@ add_action('wp_enqueue_scripts', 'mon_enqueue_css_js');
 
 function creation_carrousel()
 {
-    return ("
+    echo "
         <div class='carrousel'>
             <button class='carrousel__x'>X</button>
-            <button class='carrousel__image_pre'><</button>
-            <button class='carrousel__image_sui'>></button>
+            <button class='carrousel__image_pre'>&lt;</button>
+            <button class='carrousel__image_sui'>&gt;</button>
             <figure class='carrousel__figure'></figure>
             <form class='carrousel__form'></form>
             <figure class='carrousel__figure'></figure>
         </div> <!-- Fin du carrousel -->
-    ");
+    ";
 }
-add_shortcode('carrousel', 'creation_carrousel');
+
+function affichage_galerie()
+{
+
+    $args = array(
+        'category_name' => 'professeurs',
+        'orderby' => 'title', // Tri par titre
+        'order' => 'ASC', // Dans l'ordre alphabétique croissant
+    );
+
+    $query = new WP_Query($args);
+
+    // Affichage des articles (descriptions des professeurs)
+    if ($query->have_posts()) :
+        while ($query->have_posts()) : $query->the_post();
+            if (has_post_thumbnail()) {
+                // Affichez les miniatures des articles
+?>
+                <div>
+                    <?php the_post_thumbnail('thumbnail'); ?>
+                </div>
+<?php
+            }
+        endwhile;
+        wp_reset_postdata();
+    else :
+        echo 'Aucun article trouvé dans cette catégorie.';
+    endif;
+}
+
+
+//add_shortcode('carrousel', 'creation_carrousel');
+add_shortcode('galerie', 'affichage_galerie');
