f1666699e40ba68c4e0c6c7743ae727c894ed879
Stephanie Guerrier <e1574836@cmaisonneuve.qc.ca>
2023-11-09 12:52:45 -0500

    petit modif carroussel

--- carrousel.php
+++ carrousel.php
@@ -8,71 +8,74 @@
  * Version: 1.0.0
  */
 
-function mon_enqueue_css_js()
-{
-    $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
-    $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+// function mon_enqueue_css_js()
+// {
+//     $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+//     $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
 
-    wp_enqueue_style(
-        'tdm_plugin_carrousel_css',
-        plugin_dir_url(__FILE__) . "style.css",
-        array(),
-        $version_css
-    );
+//     wp_enqueue_style(
+//         'tdm_plugin_carrousel_css',
+//         plugin_dir_url(__FILE__) . "style.css",
+//         array(),
+//         $version_css
+//     );
 
-    wp_enqueue_script(
-        'tdm_plugin_carrousel_js',
-        plugin_dir_url(__FILE__) . "js/carrousel.js",
-        array(),
-        $version_js,
-        true
-    );
-}
-add_action('wp_enqueue_scripts', 'mon_enqueue_css_js');
+//     wp_enqueue_script(
+//         'tdm_plugin_carrousel_js',
+//         plugin_dir_url(__FILE__) . "js/carrousel.js",
+//         array(),
+//         $version_js,
+//         true
+//     );
+// }
+// add_action('wp_enqueue_scripts', 'mon_enqueue_css_js');
 
-function creation_carrousel()
-{
-    echo "
-        <div class='carrousel'>
-            <button class='carrousel__x'>X</button>
-            <button class='carrousel__image_pre'>&lt;</button>
-            <button class='carrousel__image_sui'>&gt;</button>
-            <figure class='carrousel__figure'></figure>
-            <form class='carrousel__form'></form>
-            <figure class='carrousel__figure'></figure>
-        </div> <!-- Fin du carrousel -->
-    ";
-}
+// function creation_carrousel()
+// {
+//     echo "
+//         <div class='carrousel'>
+//             <button class='carrousel__x'>X</button>
+//             <button class='carrousel__image_pre'>&lt;</button>
+//             <button class='carrousel__image_sui'>&gt;</button>
+//             <figure class='carrousel__figure'></figure>
+//             <form class='carrousel__form'></form>
+//             <figure class='carrousel__figure'></figure>
+//         </div> <!-- Fin du carrousel -->
+//     ";
+// }
 
-function affichage_galerie()
-{
+// function affichage_galerie()
+// {
+//     echo '<div class="galerie">';
+//     $args = array(
+//         'category_name' => 'professeurs',
+//         'orderby' => 'title', // Tri par titre
+//         'order' => 'ASC', // Dans l'ordre alphabétique croissant
+//     );
 
-    $args = array(
-        'category_name' => 'professeurs',
-        'orderby' => 'title', // Tri par titre
-        'order' => 'ASC', // Dans l'ordre alphabétique croissant
-    );
+//     $query = new WP_Query($args);
 
-    $query = new WP_Query($args);
-
-    // Affichage des articles (descriptions des professeurs)
-    if ($query->have_posts()) :
-        while ($query->have_posts()) : $query->the_post();
-            if (has_post_thumbnail()) {
-                // Affichez les miniatures des articles
+//     // Affichage des articles (descriptions des professeurs)
+//     if ($query->have_posts()) :
+//         while ($query->have_posts()) : $query->the_post();
+//             if (has_post_thumbnail()) {
+//                 // Affichez les miniatures des articles
+// 
 ?>
-                <div>
-                    <?php the_post_thumbnail('thumbnail'); ?>
-                </div>
-<?php
-            }
-        endwhile;
-        wp_reset_postdata();
-    else :
-        echo 'Aucun article trouvé dans cette catégorie.';
-    endif;
-}
+<!-- //                 <div>
+//                     <?php //the_post_thumbnail('thumbnail'); 
+                        ?>
+//                 </div> -->
+// <?php
+//             }
+//         endwhile;
+//         wp_reset_postdata();
+//     else :
+//         echo 'Aucun article trouvé dans cette catégorie.';
+//     endif;
+//     '</div>';
+// }
 
 
-//add_shortcode('carrousel', 'creation_carrousel');
-add_shortcode('galerie', 'affichage_galerie');
+// //add_shortcode('carrousel', 'creation_carrousel');
+// add_shortcode('galerie', 'affichage_galerie');
--- js/carrousel.js
+++ js/carrousel.js
@@ -1,129 +1,129 @@
-(function () {
-    console.log("Début du carrousel");
-    let carrousel = document.querySelector('.carrousel');
-    let carrousel__x = document.querySelector('.carrousel__x');
-    let carrousel__figure = document.querySelector('.carrousel__figure:first-of-type');
-    let carrousel__fond = document.querySelector('.carrousel__figure:last-of-type');
-    let carrousel__form = document.querySelector('.carrousel__form');
-    let carrousel__image_sui = document.querySelector('.carrousel__image_sui');
-    let carrousel__image_pre = document.querySelector('.carrousel__image_pre');
+// (function () {
+//     console.log("Début du carrousel");
+//     let carrousel = document.querySelector('.carrousel');
+//     let carrousel__x = document.querySelector('.carrousel__x');
+//     let carrousel__figure = document.querySelector('.carrousel__figure:first-of-type');
+//     let carrousel__fond = document.querySelector('.carrousel__figure:last-of-type');
+//     let carrousel__form = document.querySelector('.carrousel__form');
+//     let carrousel__image_sui = document.querySelector('.carrousel__image_sui');
+//     let carrousel__image_pre = document.querySelector('.carrousel__image_pre');
 
-    let galerie = document.querySelector('.galerie');
-    let galerie__img = galerie.querySelectorAll('img');
+//     let galerie = document.querySelector('.galerie');
+//     let galerie__img = galerie.querySelectorAll('img');
 
-    /** 
-     * On ferme le carrousel
-     */
-    carrousel__x.addEventListener('mousedown', function () {
-        carrousel.classList.remove('carrousel--activer');
-    });
+//     /**
+//      * On ferme le carrousel
+//      */
+//     carrousel__x.addEventListener('mousedown', function () {
+//         carrousel.classList.remove('carrousel--activer');
+//     });
 
-    /**
-     * On navigue vers l'image suivante
-     */
-    carrousel__image_sui.addEventListener('mousedown', function () {
-        index++;
+//     /**
+//      * On navigue vers l'image suivante
+//      */
+//     carrousel__image_sui.addEventListener('mousedown', function () {
+//         index++;
 
-        if (index == galerie__img.length) {
-            index = 0;
-        }
+//         if (index == galerie__img.length) {
+//             index = 0;
+//         }
 
-        carrousel__form.children[index].checked = true;
+//         carrousel__form.children[index].checked = true;
 
-        affiche_image_carrousel();
-    });
+//         affiche_image_carrousel();
+//     });
 
-    /**
-     * On navigue vers l'image précédente
-     */
-    carrousel__image_pre.addEventListener('mousedown', function () {
-        index--;
+//     /**
+//      * On navigue vers l'image précédente
+//      */
+//     carrousel__image_pre.addEventListener('mousedown', function () {
+//         index--;
 
-        if (index < 0) {
-            index = galerie__img.length - 1;
-        }
+//         if (index < 0) {
+//             index = galerie__img.length - 1;
+//         }
 
-        carrousel__form.children[index].checked = true;
+//         carrousel__form.children[index].checked = true;
 
-        affiche_image_carrousel(); 
-    });
+//         affiche_image_carrousel();
+//     });
 
-    /**
-     * On initialize l'index
-     */
-    let position = 0;
-    let index = 0;
-    let ancienIndex = -1;
+//     /**
+//      * On initialize l'index
+//      */
+//     let position = 0;
+//     let index = 0;
+//     let ancienIndex = -1;
 
-    /**
-     * Pour chaque image de la galerie, l'ajouter dans le carrousel
-     */
-    if (position != galerie__img.length) {
-        for (const elt of galerie__img) {
-            elt.dataset.index = position;
-            elt.addEventListener('mousedown', function () {
-                carrousel.classList.add('carrousel--activer');
-                index = this.dataset.index;
-                carrousel__form.children[index].checked = true;
-                affiche_image_carrousel();
-            });
-            ajouter_une_image_dans_carrousel(elt);
-            ajouter_une_image_de_fond(elt);
-            ajouter_un_bouton_radio_dans_carrousel();
-        }
-    }
+//     /**
+//      * Pour chaque image de la galerie, l'ajouter dans le carrousel
+//      */
+//     if (position != galerie__img.length) {
+//         for (const elt of galerie__img) {
+//             elt.dataset.index = position;
+//             elt.addEventListener('mousedown', function () {
+//                 carrousel.classList.add('carrousel--activer');
+//                 index = this.dataset.index;
+//                 carrousel__form.children[index].checked = true;
+//                 affiche_image_carrousel();
+//             });
+//             ajouter_une_image_dans_carrousel(elt);
+//             ajouter_une_image_de_fond(elt);
+//             ajouter_un_bouton_radio_dans_carrousel();
+//         }
+//     }
 
-    /**
-     * Création dynamique d'une image pour le carrousel
-     * 
-     * @param {*} elt une image de la galerie
-     */
-    function ajouter_une_image_dans_carrousel(elt) {
-        let img = document.createElement('img');
-        img.classList.add('carrousel__img');
-        img.src = elt.src;
-        carrousel__figure.appendChild(img);
-        img.addEventListener('mousedown', function () {
-            index++;
-            if (index == galerie__img.length) {
-                index = 0;
-            }
-            carrousel__form.children[index].checked = true;
-            affiche_image_carrousel();
-        });
-    }
+//     /**
+//      * Création dynamique d'une image pour le carrousel
+//      *
+//      * @param {*} elt une image de la galerie
+//      */
+//     function ajouter_une_image_dans_carrousel(elt) {
+//         let img = document.createElement('img');
+//         img.classList.add('carrousel__img');
+//         img.src = elt.src;
+//         carrousel__figure.appendChild(img);
+//         img.addEventListener('mousedown', function () {
+//             index++;
+//             if (index == galerie__img.length) {
+//                 index = 0;
+//             }
+//             carrousel__form.children[index].checked = true;
+//             affiche_image_carrousel();
+//         });
+//     }
 
-    function ajouter_une_image_de_fond(elt) {
-        let img_fond = document.createElement('img');
-        img_fond.classList.add('carrousel__img');
-        img_fond.src = elt.src;
-        carrousel__fond.appendChild(img_fond);
-    }
+//     function ajouter_une_image_de_fond(elt) {
+//         let img_fond = document.createElement('img');
+//         img_fond.classList.add('carrousel__img');
+//         img_fond.src = elt.src;
+//         carrousel__fond.appendChild(img_fond);
+//     }
 
-    function ajouter_un_bouton_radio_dans_carrousel() {
-        let rad = document.createElement('input');
-        rad.setAttribute('type', 'radio');
-        rad.setAttribute('name', 'carrousel');
-        rad.classList.add('carrousel__rad');
-        rad.dataset.index = position;
-        rad.addEventListener('mousedown', function () {
-            index = this.dataset.index;
-            affiche_image_carrousel();
-        })
-        position = position + 1;
-        carrousel__form.appendChild(rad);
-    }
+//     function ajouter_un_bouton_radio_dans_carrousel() {
+//         let rad = document.createElement('input');
+//         rad.setAttribute('type', 'radio');
+//         rad.setAttribute('name', 'carrousel');
+//         rad.classList.add('carrousel__rad');
+//         rad.dataset.index = position;
+//         rad.addEventListener('mousedown', function () {
+//             index = this.dataset.index;
+//             affiche_image_carrousel();
+//         })
+//         position = position + 1;
+//         carrousel__form.appendChild(rad);
+//     }
 
-    /**
-     * Affiche la nouvelle image du carrousel
-     */
-    function affiche_image_carrousel() {
-        if (ancienIndex != -1) {
-            carrousel__figure.children[ancienIndex].classList.remove('carrousel__img--activer');
-            carrousel__fond.children[ancienIndex].classList.remove('carrousel__fond--activer');
-        }
-        carrousel__figure.children[index].classList.add('carrousel__img--activer');
-        carrousel__fond.children[index].classList.add('carrousel__fond--activer');
-        ancienIndex = index;
-    }
-})()
+//     /**
+//      * Affiche la nouvelle image du carrousel
+//      */
+//     function affiche_image_carrousel() {
+//         if (ancienIndex != -1) {
+//             carrousel__figure.children[ancienIndex].classList.remove('carrousel__img--activer');
+//             carrousel__fond.children[ancienIndex].classList.remove('carrousel__fond--activer');
+//         }
+//         carrousel__figure.children[index].classList.add('carrousel__img--activer');
+//         carrousel__fond.children[index].classList.add('carrousel__fond--activer');
+//         ancienIndex = index;
+//     }
+// })()
