126d726c400401eb2a19bb92ec1fa9722667a230
Stephanie Guerrier <e1574836@cmaisonneuve.qc.ca>
2023-11-23 13:51:07 -0500

    Modification code php caroussel

--- carrousel.php
+++ carrousel.php
@@ -8,74 +8,77 @@
  * Version: 1.0.0
  */
 
-// function mon_enqueue_css_js()
-// {
-//     $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
-//     $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+function mon_enqueue_css_js()
+{
+    $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+    $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
 
-//     wp_enqueue_style(
-//         'tdm_plugin_carrousel_css',
-//         plugin_dir_url(__FILE__) . "style.css",
-//         array(),
-//         $version_css
-//     );
+    wp_enqueue_style(
+        'tdm_plugin_carrousel_css',
+        plugin_dir_url(__FILE__) . "style.css",
+        array(),
+        $version_css
+    );
 
-//     wp_enqueue_script(
-//         'tdm_plugin_carrousel_js',
-//         plugin_dir_url(__FILE__) . "js/carrousel.js",
-//         array(),
-//         $version_js,
-//         true
-//     );
-// }
-// add_action('wp_enqueue_scripts', 'mon_enqueue_css_js');
+    wp_enqueue_script(
+        'tdm_plugin_carrousel_js',
+        plugin_dir_url(__FILE__) . "js/carrousel.js",
+        array(),
+        $version_js,
+        true
+    );
+}
+add_action('wp_enqueue_scripts', 'mon_enqueue_css_js');
 
-// function creation_carrousel()
-// {
-//     echo "
-//         <div class='carrousel'>
-//             <button class='carrousel__x'>X</button>
-//             <button class='carrousel__image_pre'>&lt;</button>
-//             <button class='carrousel__image_sui'>&gt;</button>
-//             <figure class='carrousel__figure'></figure>
-//             <form class='carrousel__form'></form>
-//             <figure class='carrousel__figure'></figure>
-//         </div> <!-- Fin du carrousel -->
-//     ";
-// }
+//Zone carrousel d'images des professeurs 
+function affichage_galerie()
+{
+    $query = new WP_Query(array('category_name' => 'professeurs'));
 
-// function affichage_galerie()
-// {
-//     echo '<div class="galerie">';
-//     $args = array(
-//         'category_name' => 'professeurs',
-//         'orderby' => 'title', // Tri par titre
-//         'order' => 'ASC', // Dans l'ordre alphabétique croissant
-//     );
-
-//     $query = new WP_Query($args);
-
-//     // Affichage des articles (descriptions des professeurs)
-//     if ($query->have_posts()) :
-//         while ($query->have_posts()) : $query->the_post();
-//             if (has_post_thumbnail()) {
-//                 // Affichez les miniatures des articles
-// 
+    if ($query->have_posts()) :
 ?>
-<!-- //                 <div>
-//                     <?php //the_post_thumbnail('thumbnail'); 
-                        ?>
-//                 </div> -->
-// <?php
-//             }
-//         endwhile;
-//         wp_reset_postdata();
-//     else :
-//         echo 'Aucun article trouvé dans cette catégorie.';
-//     endif;
-//     '</div>';
-// }
+        <div class="mainProf">
+            <div class="carousel">
+                <button onclick="prec()" class="precedent">&lt;</button>
+                <div class="zoneProfs">
+                    <?php
+                    while ($query->have_posts()) : $query->the_post();
 
+                    ?>
+                        <figure class="prof__figure" id="fig_<?php the_id() ?>">
+                            <?php the_post_thumbnail('thumbnail'); ?>
+                        </figure>
+                    <?php
+                    endwhile;
+                    ?>
+                </div>
+                <button onclick="suiv()" class="suivant">&gt;</button>
+            </div>
+        </div>
+        <!-- Zone textes des professeurs -->
+        <div class="description">
+            <?php
+            $query = new WP_Query(array('category_name' => 'professeurs'));
 
-// //add_shortcode('carrousel', 'creation_carrousel');
-// add_shortcode('galerie', 'affichage_galerie');
+            if ($query->have_posts()) :
+                while ($query->have_posts()) : $query->the_post();
+            ?>
+                    <section class="prof__section" id="des_<?php the_id(); ?>">
+                        <figure class="prof__figure" id="fig_<?php the_id() ?>">
+                            <?php the_post_thumbnail('thumbnail'); ?>
+                        </figure>
+                        <?php the_content(); ?>
+                    </section>
+            <?php
+                endwhile;
+            endif;
+            wp_reset_postdata();
+            ?>
+        </div>
+        <form class="radiobouton"></form>
+<?php
+    endif;
+    wp_reset_postdata();
+}
+
+add_shortcode('galerie', 'affichage_galerie');
--- js/carrousel.js
+++ js/carrousel.js
@@ -1,129 +1,113 @@
-// (function () {
-//     console.log("Début du carrousel");
-//     let carrousel = document.querySelector('.carrousel');
-//     let carrousel__x = document.querySelector('.carrousel__x');
-//     let carrousel__figure = document.querySelector('.carrousel__figure:first-of-type');
-//     let carrousel__fond = document.querySelector('.carrousel__figure:last-of-type');
-//     let carrousel__form = document.querySelector('.carrousel__form');
-//     let carrousel__image_sui = document.querySelector('.carrousel__image_sui');
-//     let carrousel__image_pre = document.querySelector('.carrousel__image_pre');
+(function () {
+  console.log("vive les profs du TIM");
+  let elm__fig = document.querySelectorAll(".prof__figure");
+  console.log(elm__fig.length);
 
-//     let galerie = document.querySelector('.galerie');
-//     let galerie__img = galerie.querySelectorAll('img');
+  let elm__prof__section = document.querySelectorAll(".prof__section");
+  for (let elm__prof of elm__prof__section) {
+    elm__prof.style.display = "none";
+  }
 
-//     /**
-//      * On ferme le carrousel
-//      */
-//     carrousel__x.addEventListener('mousedown', function () {
-//         carrousel.classList.remove('carrousel--activer');
-//     });
+  for (const elm of elm__fig) {
+    elm.addEventListener("mousedown", function () {
+      let elm__prof__section = document.querySelectorAll(".prof__section");
+      for (let elm__prof of elm__prof__section) {
+        elm__prof.style.display = "none";
+      }
 
-//     /**
-//      * On navigue vers l'image suivante
-//      */
-//     carrousel__image_sui.addEventListener('mousedown', function () {
-//         index++;
+      id__figure = elm.id.split("_")[1]; // "fig_234"  ["fig", "234"]
+      // id__figure = elm.id
+      console.log(id__figure);
+      elm__prof__section = document.querySelector("#des_" + id__figure);
+      console.log(elm__prof__section.id);
+      elm__prof__section.style.display = "block";
+    });
+  }
+})();
 
-//         if (index == galerie__img.length) {
-//             index = 0;
-//         }
+//[VARIABLES]
+const zoneProfs = document.querySelector(".zoneProfs");
+const formRadios = document.querySelector(".radiobouton");
+const images = document.querySelectorAll(".prof__figure");
+let deplacement = 0;
+const limiteGauche = 0;
+let section;
+let limiteDroite;
 
-//         carrousel__form.children[index].checked = true;
+//[CONSOLE LOG]
+console.log(images.length / 3);
+console.log(section);
 
-//         affiche_image_carrousel();
-//     });
+// Fonction pour afficher les boutons radio en fonction du nombre d'images
+function afficherBoutonsRadio() {
+  section = Math.ceil(images.length / 3); // Calcule le nombre de boutons radio à afficher  (arrondi supérieur)
+  const indexImageActuelle = Math.abs(deplacement / 100) + 1;
+  formRadios.innerHTML = ""; // Vide la zone des boutons radio
+  // Crée un bouton radio pour chaque section
+  for (let i = 1; i <= section; i++) {
+    const inputRadio = document.createElement("input");
+    inputRadio.type = "radio";
+    inputRadio.name = "radioProf";
+    inputRadio.value = i;
+    inputRadio.classList.add(`radio${i}`);
+    formRadios.appendChild(inputRadio);
+  }
+}
 
-//     /**
-//      * On navigue vers l'image précédente
-//      */
-//     carrousel__image_pre.addEventListener('mousedown', function () {
-//         index--;
+// Fonction pour mettre à jour les boutons radio en fonction du déplacement
+function mettreAJourBoutonRadio() {
+  const indexImageActuelle = Math.abs(deplacement / 100) + 1; // Calcule l'index de l'image actuelle
+  const radios = formRadios.querySelectorAll("input"); // Sélectionne tous les boutons radio
+  radios.forEach((radio, index) => {
+    // Pour chaque bouton radio
+    radio.checked = index + 1 === indexImageActuelle; // Coche le bouton radio correspondant à l'image actuelle
+    console.log(indexImageActuelle);
+  });
+}
 
-//         if (index < 0) {
-//             index = galerie__img.length - 1;
-//         }
+// Fonction pour déplacer le carousel vers la droite
+function suiv() {
+  limiteDroite = section * -100;
+  console.log(section);
+  if (deplacement > limiteDroite + 100) {
+    // Si le déplacement est plus petit que la limite de droite
+    // On déplace le carousel vers la droite
+    deplacement -= 100;
+    zoneProfs.style.transform = `translateX(${deplacement}%)`;
+    mettreAJourBoutonRadio();
+  }
+}
 
-//         carrousel__form.children[index].checked = true;
+// Fonction pour déplacer le carousel vers la gauche
+function prec() {
+  section = Math.floor(images.length / 3);
+  if (deplacement < limiteGauche) {
+    // Si le déplacement est plus grand que la limite de gauche
+    // On déplace le carousel vers la gauche
+    deplacement += 100;
+    zoneProfs.style.transform = `translateX(${deplacement}%)`;
+    mettreAJourBoutonRadio();
+  }
+}
 
-//         affiche_image_carrousel();
-//     });
+// Fonction pour afficher l'image en fonction du bouton radio clique
+function afficherImage(index) {
+  deplacement = -(index - 1) * 100;
+  zoneProfs.style.transform = `translateX(${deplacement}%)`;
+}
 
-//     /**
-//      * On initialize l'index
-//      */
-//     let position = 0;
-//     let index = 0;
-//     let ancienIndex = -1;
+// Fonction pour ajouter un événement de clic à chaque bouton radio
+function ajouterEvenementBoutonRadio() {
+  const radios = formRadios.querySelectorAll("input"); // Sélectionne tous les boutons radio
+  radios.forEach((radio, index) => {
+    // Pour chaque bouton radio
+    radio.addEventListener("click", () => {
+      // Ajoute un événement de clic
+      afficherImage(index + 1); // Affiche l'image correspondante
+    });
+  });
+}
 
-//     /**
-//      * Pour chaque image de la galerie, l'ajouter dans le carrousel
-//      */
-//     if (position != galerie__img.length) {
-//         for (const elt of galerie__img) {
-//             elt.dataset.index = position;
-//             elt.addEventListener('mousedown', function () {
-//                 carrousel.classList.add('carrousel--activer');
-//                 index = this.dataset.index;
-//                 carrousel__form.children[index].checked = true;
-//                 affiche_image_carrousel();
-//             });
-//             ajouter_une_image_dans_carrousel(elt);
-//             ajouter_une_image_de_fond(elt);
-//             ajouter_un_bouton_radio_dans_carrousel();
-//         }
-//     }
-
-//     /**
-//      * Création dynamique d'une image pour le carrousel
-//      *
-//      * @param {*} elt une image de la galerie
-//      */
-//     function ajouter_une_image_dans_carrousel(elt) {
-//         let img = document.createElement('img');
-//         img.classList.add('carrousel__img');
-//         img.src = elt.src;
-//         carrousel__figure.appendChild(img);
-//         img.addEventListener('mousedown', function () {
-//             index++;
-//             if (index == galerie__img.length) {
-//                 index = 0;
-//             }
-//             carrousel__form.children[index].checked = true;
-//             affiche_image_carrousel();
-//         });
-//     }
-
-//     function ajouter_une_image_de_fond(elt) {
-//         let img_fond = document.createElement('img');
-//         img_fond.classList.add('carrousel__img');
-//         img_fond.src = elt.src;
-//         carrousel__fond.appendChild(img_fond);
-//     }
-
-//     function ajouter_un_bouton_radio_dans_carrousel() {
-//         let rad = document.createElement('input');
-//         rad.setAttribute('type', 'radio');
-//         rad.setAttribute('name', 'carrousel');
-//         rad.classList.add('carrousel__rad');
-//         rad.dataset.index = position;
-//         rad.addEventListener('mousedown', function () {
-//             index = this.dataset.index;
-//             affiche_image_carrousel();
-//         })
-//         position = position + 1;
-//         carrousel__form.appendChild(rad);
-//     }
-
-//     /**
-//      * Affiche la nouvelle image du carrousel
-//      */
-//     function affiche_image_carrousel() {
-//         if (ancienIndex != -1) {
-//             carrousel__figure.children[ancienIndex].classList.remove('carrousel__img--activer');
-//             carrousel__fond.children[ancienIndex].classList.remove('carrousel__fond--activer');
-//         }
-//         carrousel__figure.children[index].classList.add('carrousel__img--activer');
-//         carrousel__fond.children[index].classList.add('carrousel__fond--activer');
-//         ancienIndex = index;
-//     }
-// })()
+// Appelle la fonction d'affichage des boutons radio au chargement de la page
+afficherBoutonsRadio();
+ajouterEvenementBoutonRadio();
