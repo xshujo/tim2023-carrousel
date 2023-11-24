fd932b91dd088589f94bfd0bce55a2f5786f7732
Stephanie Guerrier <e1574836@cmaisonneuve.qc.ca>
2023-11-24 09:00:43 -0500

    Modification design des boutons de défilement

--- carrousel.php
+++ carrousel.php
@@ -39,7 +39,9 @@ function affichage_galerie()
 ?>
         <div class="mainProf">
             <div class="carousel">
-                <button onclick="prec()" class="precedent">&lt;</button>
+                <button onclick="prec()" class="precedent"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                        <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
+                    </svg></button>
                 <div class="zoneProfs">
                     <?php
                     while ($query->have_posts()) : $query->the_post();
@@ -52,7 +54,9 @@ function affichage_galerie()
                     endwhile;
                     ?>
                 </div>
-                <button onclick="suiv()" class="suivant">&gt;</button>
+                <button onclick="suiv()" class="suivant"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                        <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+                    </svg></button>
             </div>
         </div>
         <!-- Zone textes des professeurs -->
